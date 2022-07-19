6afba1e516125cd2b90f455bec1f453cc5d15ce9
admin <admin>
2022-07-19 16:11:06 +0800

    Recipe MLFLOW.compute_cTcsiBqc saved through notebook notebook editor for compute_cTcsiBqc

--- recipes/compute_cTcsiBqc.py
+++ recipes/compute_cTcsiBqc.py
@@ -2,8 +2,8 @@
 import os
 import shutil
 import dataiku
-
-from dataiku import recipe
+import pandas as pd
+import mlflow
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 client = dataiku.api_client()
@@ -11,56 +11,81 @@ project = client.get_default_project()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # get train dataset
-train_dataset = recipe.get_inputs_as_datasets()[0]
-evaluation_dataset = recipe.get_inputs_as_datasets()[1]
+train_dataset = dataiku.Dataset("training_data")
+evaluation_dataset = dataiku.Dataset("eval_data")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # get output saved model
-sm = project.get_saved_model(recipe.get_output_names()[0])
+sm = project.get_saved_model("VdHxdbkg")
 
 # get train dataset as a pandas dataframe
 df = train_dataset.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # get the path of a local managed folder where to temporarily save the trained model
-mf = dataiku.Folder("local_managed_folder")
+mf = dataiku.Folder("cTcsiBqc")
 path = mf.get_path()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 model_subdir = "my_subdir"
 model_dir = os.path.join(path, model_subdir)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+from sklearn.linear_model import LogisticRegression
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+y = df['y']
+X = df.drop('y', axis=1)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+job_df = pd.get_dummies(df.job, prefix='job')
+marital_df = pd.get_dummies(df.marital, prefix='marital')
+education_df = pd.get_dummies(df.education, prefix='education')
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+month_dic = {
+    'apr': 5,
+    'aug': 8,
+    'dec': 12,
+    'feb': 2,
+    'jan': 1,
+    'jul': 7,
+    'jun': 6,
+    'mar': 3,
+    'may': 5,
+    'nov': 11,
+    'oct': 10,
+    'sep': 9}
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+X.drop(['job', 'marital', 'education'], axis=1, inplace=True)
+X = pd.concat([X, job_df, marital_df, education_df], axis=1)
+X.month.replace(month_dic, inplace=True)
+X.replace({"no": 0, "yes": 1}, inplace=True)
+clf = LogisticRegression(random_state=0).fit(X, y)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 if os.path.exists(model_dir):
     shutil.rmtree(model_dir)
 
 try:
     # ...train your model...
-
+    clf = LogisticRegression(random_state=0).fit(X, y)
+    
     # ...save it with package specific MLflow method (here, SKlearn)...
-    mlflow.sklearn.save_model(my_model, model_dir)
+    mlflow.sklearn.save_model(clf, model_dir)
 
     # import the model, creating a new version
-    mlflow_version = sm.import_mlflow_version_from_managed_folder("version_name", "local_managed_folder", model_subdir, "code-env-with-mlflow-name")
+    mlflow_version = sm.import_mlflow_version_from_managed_folder("v03", "cTcsiBqc", model_subdir, "py36_mlflow")
 finally:
     shutil.rmtree(model_dir)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # setting metadata (target name, classes,...)
-mlflow_version.set_core_metadata(target_column, ["class0", "class1",...] , get_features_from_dataset=evaluation_dataset.name)
-
-# evaluate the performance of this new version, to populate the performance screens of the saved model version in DSS
-mlflow_version.evaluate(evaluation_dataset.name)
+mlflow_version.set_core_metadata(target_column_name="y",
+                             class_labels=["no", "yes"],
+                             get_features_from_dataset="eval_data_prepared")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# -*- coding: utf-8 -*-
-import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
-
-# Read recipe inputs
-training_data = dataiku.Dataset("training_data")
-training_data_df = training_data.get_dataframe()
-
-
-
-
-# Write recipe outputs
-MLflow_model = dataiku.Folder("cTcsiBqc")
-MLflow_model_info = MLflow_model.get_info()
+# evaluate the performance of this new version, to populate the performance screens of the saved model version in DSS
+mlflow_version.evaluate("eval_data_prepared")
