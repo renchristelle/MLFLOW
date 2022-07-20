fc5787922d6dee1543180ae08f91f49a650d32e4
admin <admin>
2022-07-20 17:49:52 +0800

    Recipe MLFLOW.compute_cTcsiBqc saved through notebook notebook editor for compute_cTcsiBqc

--- recipes/compute_cTcsiBqc.py
+++ recipes/compute_cTcsiBqc.py
@@ -4,6 +4,10 @@ import shutil
 import dataiku
 import pandas as pd
 import mlflow
+from sklearn.linear_model import LogisticRegression
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+from dss_mlflow import preprocessing
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 client = dataiku.api_client()
@@ -26,43 +30,11 @@ df = train_dataset.get_dataframe()
 mf = dataiku.Folder("cTcsiBqc")
 path = mf.get_path()
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 model_subdir = "my_subdir"
 model_dir = os.path.join(path, model_subdir)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-from sklearn.linear_model import LogisticRegression
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-y = df['y']
-X = df.drop('y', axis=1)
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-job_df = pd.get_dummies(df.job, prefix='job')
-marital_df = pd.get_dummies(df.marital, prefix='marital')
-education_df = pd.get_dummies(df.education, prefix='education')
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-month_dic = {
-    'apr': 5,
-    'aug': 8,
-    'dec': 12,
-    'feb': 2,
-    'jan': 1,
-    'jul': 7,
-    'jun': 6,
-    'mar': 3,
-    'may': 5,
-    'nov': 11,
-    'oct': 10,
-    'sep': 9}
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-X.drop(['job', 'marital', 'education'], axis=1, inplace=True)
-X = pd.concat([X, job_df, marital_df, education_df], axis=1)
-X.month.replace(month_dic, inplace=True)
-X.replace({"no": 0, "yes": 1}, inplace=True)
-clf = LogisticRegression(random_state=0).fit(X, y)
+X, y = preprocessing.clean_df(df)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 if os.path.exists(model_dir):
@@ -71,7 +43,7 @@ if os.path.exists(model_dir):
 try:
     # ...train your model...
     clf = LogisticRegression(random_state=0).fit(X, y)
-    
+
     # ...save it with package specific MLflow method (here, SKlearn)...
     mlflow.sklearn.save_model(clf, model_dir)
 
