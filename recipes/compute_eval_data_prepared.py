8d97a85cba64e8cb1c99ac92e82769d60fbdeefd
admin <admin>
2022-07-20 17:51:23 +0800

    Saved recipe: MLFLOW.compute_eval_data_prepared

--- recipes/compute_eval_data_prepared.py
+++ recipes/compute_eval_data_prepared.py
@@ -2,36 +2,13 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+from dss_mlflow import preprocessing
 
 # Read recipe inputs
 eval_data_ = dataiku.Dataset("eval_data_")
 df = eval_data_.get_dataframe()
 
-y = df['y']
-X = df.drop('y', axis=1)
-
-job_df = pd.get_dummies(df.job, prefix='job')
-marital_df = pd.get_dummies(df.marital, prefix='marital')
-education_df = pd.get_dummies(df.education, prefix='education')
-
-month_dic = {
-    'apr': 5,
-    'aug': 8,
-    'dec': 12,
-    'feb': 2,
-    'jan': 1,
-    'jul': 7,
-    'jun': 6,
-    'mar': 3,
-    'may': 5,
-    'nov': 11,
-    'oct': 10,
-    'sep': 9}
-
-X.drop(['job', 'marital', 'education'], axis=1, inplace=True)
-X = pd.concat([X, job_df, marital_df, education_df], axis=1)
-X.month.replace(month_dic, inplace=True)
-X.replace({"no": 0, "yes": 1}, inplace=True)
+X, y = preprocessing.clean_df(df)
 
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
